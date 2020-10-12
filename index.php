de9ca8ec80b7c4e279549433237804ef53f62424
AhMing <keeshengming@outlook.com>
2020-10-12 22:58:48 +0800

    Initial Release

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Ecolla Homepage</title>
+    </head>
+    <body>
+        
+        <!-- Declare the Object Needed -->
+        <?php //include "item-properties.php" ?>
+
+        <?php 
+            $title = "Ecolla ABC";
+            include "header.php"; 
+        ?>
+        <div>
+        <?php ?>
+        </div>
+
+
+        
+        <?php include "footer.php"; ?>
+
+    </body>
+</html>
