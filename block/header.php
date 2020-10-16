e182ac77fc0cf3e2cf679f25a4b836977cf0fa80
J0u0J <72692004+J0u0J@users.noreply.github.com>
2020-10-16 13:38:59 +0800

    ecolla icon on navbar

--- block/header.php
+++ block/header.php
@@ -1,8 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="index.php">
-            <img src="../assets/images/icon/ecolla_icon.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+        <div class="icon">
+            <img src="assets/images/icon/ecolla_icon.png" width="50" height="50" overflow="hidden" loading="lazy">
             ε口乐
+        </div>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
