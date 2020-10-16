b8650ca0aa98b1798c8285db7c81a6c6da4bcb00
AhMing <keeshengming@outlook.com>
2020-10-16 14:57:55 +0800

    Comment rearranged.

--- block/carousel-block.php
+++ block/carousel-block.php
@@ -1,17 +1,21 @@
 <div id="imgSlide" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
         <?php 
+        
             for($i = 0; $i < sizeof($imgList); $i++){
                 echo "<li data-target=\"#imgSlide\" data-slide-to=\"".$i."\"";
                 if($i == 0) echo " class=\"active\""; //Set active for the first slide
                 echo "></li>";
             }
+
             // Original Pattern: <li data-target="#imgSlide" data-slide-to="{slide index}"></li>
+            
         ?>
     </ol>
     
     <div class="carousel-inner">
         <?php 
+
             for($i = 0; $i < sizeof($imgList); $i++){
                 echo "<div class=\"carousel-item";
                 if($i == 0) echo " active"; //Set active for the first slide
@@ -19,8 +23,8 @@
                 echo "<img class=\"d-block w-100\" src=\"".$imgList[$i]."\" data-interval=\"10000\">";
                 echo "</div>";
             }
-            //Original Pattern
-            /*
+
+            /* Original Pattern
             <div class="carousel-item">
                 <img class="d-block w-100" src={img directory} data-interval="10000">
             </div>
