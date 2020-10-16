53071742412ab7ba9947953e70666f60237c1d0f
AhMing <keeshengming@outlook.com>
2020-10-16 22:57:59 +0800

    Item page template updated.

--- items/item-page-template.php
+++ items/item-page-template.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title><?php echo "$itemName" ?> | Ecolla ε口乐</title>
+    <title><?php echo "$item->getName()" ?> | Ecolla ε口乐</title>
 </head>
 
 <body>
@@ -12,8 +12,47 @@
 
     <?php include "../block/header.php"; ?>
 
-    <div>
 
+
+    <div class="container">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="../item-list.php">商品列表</a></li>
+                <li class="breadcrumb-item">
+                    <a href="../items-list.php?catogory=<?php echo strtolower($item->getCatogory()) ?>">
+                        <?php echo strtolower($item->getCatogory()) ?>
+                    </a>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page"><?php echo $item->getName(); ?></li>
+            </ol>
+
+        </nav>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+                <!-- Item Picture Carousel -->
+                <div class="col-xs-0 col-sm-0 col-md-0 col-lg-12">
+                    <!-- Picture Selection for the Carousel -->
+                </div>
+            </div>
+            
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
+                <!-- Item Information -->
+            </div>
+        </div>
+
+        <div class="col-12">
+            <!-- Item Description -->
+        </div>
+
+        <div class="row">
+            <!-- Show Items with same catogory -->
+            
+            <?php
+                $itemList = array();
+                include "../block/item-block.php";
+            ?>
+        </div>
     </div>
 
     <?php include "footer.php"; ?>
