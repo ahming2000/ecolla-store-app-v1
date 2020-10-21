878a9537a92205f7bed2f2f4162fb057270e61ea
AhMing <keeshengming@outlook.com>
2020-10-21 18:48:58 +0800

    Minor bug fixed.

--- assets/php/includes/class-auto-loader.inc.php
+++ assets/php/includes/class-auto-loader.inc.php
@@ -7,7 +7,7 @@
 spl_autoload_register('classAutoLoader');
 
 function classAutoLoader($className){
-    $path = "../assets/php/classes/";
+    $path = "assets/php/classes/";
     $extention = ".class.php";
     $fullPath = $path.$className.$extention;
 
