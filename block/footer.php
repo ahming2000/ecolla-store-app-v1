edaa60833e357990e10bfdf1433ccd96e086e80c
J0u0J <72692004+J0u0J@users.noreply.github.com>
2020-10-24 16:11:04 +0800

    update footer

--- block/footer.php
+++ block/footer.php
@@ -1,23 +1,39 @@
-<footer class="page-footer pt-5">
+<footer class="pt-5">
 <div class="p-4" style="background-color:#3c3e44;">
-    <div class="container text-center text-md-left">
-        <div class="row justify-content-md-center">
-        <div class="col mx-auto">
+    <div class="container">
+        <div class="row">
+        <div class="col pr-3">
             <div class="container-fluid">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d994.610069720882!2d101.14378741460057!3d4.328107998352038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cae2bc71984c77%3A0x85e97a678280a2b!2s2365%2C%20Jalan%20Hala%20Timah%203%2C%20Taman%20Kolej%20Perdana%2C%2031900%20Kampar%2C%20Negeri%20Perak!5e0!3m2!1sen!2smy!4v1603436959003!5m2!1sen!2smy"
             width="400" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
         </div>
 
-            <div class="col mx-auto">
+        <div class="col pl-5">
+        <h4 class="font-color font-weight-bold">Locate Us</h4>
+        <hr class="footer-hr">
+        <p class="text-uppercase font-color">2365, Jalan Hala Timah 3 <br>Taman Kolej Perdana 
+        <br>31900 Kampar Negeri Perak</p>
+        </div>
+
+        <div class="col">
             <h4 class='font-color font-weight-bold'>Contact Us</h4>
             <hr class="footer-hr">
-                <p class="text-uppercase font-color"><i class="fas fa-home pr-2"></i>jln123, perak</p>
                 <p class="font-color"><i class="fas fa-phone pr-2"></i>012-3456789</p>
                 <p class="font-color"><i class="fab fa-facebook-square pr-2"></i>Ecolla e口乐</p>
-            </div>
+        </div>
 
+    </div>
+    </div>
+    </div>
+
+    <div class="row pt-3 pb-3 logo-bt">
+        <div class="col">
+            <div class="text-center">
+            <img src="assets/images/icon/ecolla_icon.png" width="50" height="50" alt="logo" loading="lazy">
+            <span class="font-color">ε口乐</span>
+            </div>
         </div>
     </div>
-</div>
+
 </footer>
