cc8dddf26e50d5d17064dfa324a09272960c3b97
J0u0J <72692004+J0u0J@users.noreply.github.com>
2020-10-26 13:14:12 +0800

    item-example

--- items/example-item.php
+++ items/example-item.php
@@ -3,23 +3,30 @@
 <head>
     <meta charset="utf-8">
     <title> | Ecolla ε口乐</title>
-     <link rel="stylesheet" href="../assets/vendor/bootstrap-4.5.2-dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../assets/vendor/icofont/icofont.min.css">
-        <link rel="shortcut icon" href="../assets/images/icon/ecollafavicon.ico">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link rel="shortcut icon" href="../assets/images/icon/ecollafavicon.ico">
+    <link rel="stylesheet" href="../assets/vendor/bootstrap-4.5.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/vendor/icofont/icofont.min.css">
+    <link rel="stylesheet" href="../deco.css">
 </head>
 <body>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="../assets/vendor/bootstrap-4.5.2-dist/js/bootstrap.min.js"></script>        
-    <?php include "../assets/php/classes.php"; ?>
-    <?php include "../block/header.php"; ?>
+    <script src="assets/vendor/bootstrap-4.5.2-dist/js/bootstrap.min.js"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>        
+    
+    <?php include __DIR__."\\..\\block\\header.php"; ?>
+
+    <wrapper class="d-flex flex-column">
+    <main class="flex-fill"> <!--put content-->
+
     <section class="mb-5">
     
     </section>
     <div class="container">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="../item-list.php">商品列表</a></li>
+                <li class="breadcrumb-item"><a href="#">商品列表</a></li>
                 <li class="breadcrumb-item">
                     <a href="#">Catogory</a>
                 </li>
@@ -49,8 +56,11 @@
         </div>
     </div>
 
+    </main>
+
     <?php include "../block/footer.php"; ?>
 
+</wrapper> 
 </body>
 
 </html>
