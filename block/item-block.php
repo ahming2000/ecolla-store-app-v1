0e4898195c6c22657b4f15e839d9d20f0979919f
AhMing <keeshengming@outlook.com>
2020-10-27 19:58:24 +0800

    Price viewing format updated.

--- block/item-block.php
+++ block/item-block.php
@@ -3,7 +3,7 @@
         <img src="<?php echo $item->getImgPaths()[0]; ?>" class="card-img-top" alt="image">
         <div class="card-body">
             <h5 class="card-title"><?php echo $item->getName() ?></h5>
-            <span><?php echo "RM".$item->getVarieties()[0]->getPrice() ?></span>
+            <span><?php echo "RM".number_format($item->getVarieties()[0]->getPrice(), 2) ?></span>
         </div>
     </div>
 </div>
