6c06a847620132f860db60d0c5aa8ec390552f6b
AhMing <keeshengming@outlook.com>
2020-10-30 17:33:58 +0800

    Item list page now can detect unlist item and only list the listed item.

--- item-list.php
+++ item-list.php
@@ -15,30 +15,32 @@
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="assets/vendor/bootstrap-4.5.2-dist/js/bootstrap.min.js"></script>
-        <script src='https://kit.fontawesome.com/a076d05399.js'></script>        
-        
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
         <?php include "block/header.php"; ?>
-        
+
     <wrapper class="d-flex flex-column">
     <main class="flex-fill">
 
         <div class="container">
             <div class="row">
-            
+
                 <?php
                     $view = new View();
                     $itemList = $view->getAllItems(); //Important!! Need to take the data from the database!!!
-                    
+
                     foreach($itemList as $i){
                         $item = $i;
-                        include "block/item-block.php";    
+                        if($item->isListed()){
+                            include "block/item-block.php";
+                        }
                     }
-                    
+
                 ?>
 
             </div>
         </div>
-        
+
     </main>
 
         <section>
@@ -52,9 +54,7 @@
 
             });
         </script>
-    
+
     </wrapper>
     </body>
 </html>
-
-
