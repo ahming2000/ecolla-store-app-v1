2bbef66103fd6043379578b8292a7e334156d29d
AhMing <keeshengming@outlook.com>
2020-11-10 14:16:19 +0800

    Example item 2 and 3 uploaded. Example item page markup updated.

--- block/item-block.php
+++ block/item-block.php
@@ -1,6 +1,8 @@
 <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
     <div class="card">
-        <img src="<?php echo $item->getImgPaths()[0]; ?>" class="card-img-top" alt="image">
+        <a href="items/<?php echo $item->getCatogory() ?>/<?php echo $item->getBrand() ?>-<?php echo $item->getName() ?>">
+            <img src="<?php echo $item->getImgPaths()[0]; ?>" class="card-img-top" alt="image">
+        </a>
         <div class="card-body">
             <h5 class="card-title"><?php echo $item->getName() ?></h5>
             <span><?php echo "RM".number_format($item->getVarieties()[0]->getPrice(), 2) ?></span>
