f6bdd1f29e78858984aec7f7f374644ae430a282
J0u0J <72692004+J0u0J@users.noreply.github.com>
2020-11-13 11:19:23 +0800

    owl carousel is better!!!!!!!!!!!!

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
         <link rel="stylesheet" href="assets/vendor/bootstrap-4.5.2-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/vendor/icofont/icofont.min.css">
         <link rel="stylesheet" href="deco.css">
+        <link rel="stylesheet" href="assets/vendor/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css" />
+        <link rel="stylesheet" href="assets/vendor/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css" />
         
     </head>
     <style>/*overwrite deco.css*/
@@ -33,6 +35,7 @@
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="assets/vendor/bootstrap-4.5.2-dist/js/bootstrap.min.js"></script>
         <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+        <script src="assets/vendor/OwlCarousel2-2.3.4/dist/owl.carousel.min.js"></script>
        
     <wrapper class="d-flex flex-column">
     <main class="flex-fill"> <!--put content-->
@@ -53,15 +56,18 @@
         <section class="section1">
             <div class="container mt-5">
                 <h3 class="pt-3 pl-5">New !</h3>
-            <?php include "block/item-carousel.php";?>
+                <div class="owl-carousel owl-theme">
+                    <?php include "block/item-carousel.php";?>
+                </div>
             </div>
         </section>
 
         <section class="section2">
-            <div class="container mt-5">
-                <h3 class="pt-3 pl-5">Hot Sell !</h3>
-            <?php include "block/item-carousel.php";?>
-            </div>
+        <div class="container mt-5">
+                <h3 class="pt-3 pl-5">Hot !</h3>
+                <div class="owl-carousel owl-theme">
+                    <?php include "block/item-carousel.php";?>
+                </div>
         </section>
 
     </main>
@@ -75,6 +81,10 @@
 
                 $("#cartCount").html("0");
 
+            });
+
+            $(document).ready(function(){
+            $(".owl-carousel").owlCarousel();
             });
         </script>
     
