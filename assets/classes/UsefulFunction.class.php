722dee59c60ad07cadfc0f204601bfddc8d8415e
AhMing <keeshengming@outlook.com>
2020-12-01 09:48:58 +0800

    File upload function for item updated.

--- assets/classes/UsefulFunction.class.php
+++ assets/classes/UsefulFunction.class.php
@@ -46,71 +46,184 @@ class UsefulFunction{
         }
     }
 
-    public static function generateSolidBackground(){
-        // Get height and width of the image
-        // Generate solid background (black)
-        // Invert the color
-        // Return img file path or return img file
-    }
+    public static function cropImageToSquare($imgPath, $mode){
 
-    public static function cropImageToSquare(){
-        // Two option: add frame or crop directly
-        // Recieve optimized source image and white background with same size of source image
-    }
+        $info = GetImageSize($imgPath);
+        $width = $info[0];
+        $height = $info[1];
+        $mime = $info['mime'];
 
-    public static function optimizeImage($imgPath, $orgFileExtention){
-        $img = imagecreatefromjpeg($imgPath.".".$orgFileExtention);
-        imagejpeg ($img, $imgPath.".jpg", 75);
-    }
+        if($width == $height) {
+            return;
+        } else{
 
-    public static function uploadReceipt($filePtr, $orderId){
-        // To-do: if it is jpg file, convert to png file
-        echo UsefulFunction::upload($filePtr, "assets/images/orders/", $orderId);
-        UsefulFunction::optimizeImage("assets/images/orders/".$orderId, strtolower(pathinfo($filePtr["name"], PATHINFO_EXTENSION)));
-    }
+            $type = substr(strrchr($mime, '/'), 1);
 
-    public static function uploadItemImage(){
+            switch ($type){
+                case 'jpeg':
+                $image_create_func = 'ImageCreateFromJPEG';
+                $image_save_func = 'ImageJPEG';
+                $new_image_ext = 'jpg';
+                break;
 
-        // Get image heigh and width
-        // Generate white background
-        // crop image to square and combine with white background
+                case 'png':
+                $image_create_func = 'ImageCreateFromPNG';
+                $image_save_func = 'ImagePNG';
+                $new_image_ext = 'png';
+                break;
+
+                case 'bmp':
+                $image_create_func = 'ImageCreateFromBMP';
+                $image_save_func = 'ImageBMP';
+                $new_image_ext = 'bmp';
+                break;
+
+                case 'gif':
+                    $image_create_func = 'ImageCreateFromGIF';
+                    $image_save_func = 'ImageGIF';
+                    $new_image_ext = 'gif';
+                    break;
+
+                    case 'vnd.wap.wbmp':
+                    $image_create_func = 'ImageCreateFromWBMP';
+                    $image_save_func = 'ImageWBMP';
+                    $new_image_ext = 'bmp';
+                    break;
+
+                    case 'xbm':
+                    $image_create_func = 'ImageCreateFromXBM';
+                    $image_save_func = 'ImageXBM';
+                    $new_image_ext = 'xbm';
+                    break;
+
+                    default:
+                    $image_create_func = 'ImageCreateFromJPEG';
+                    $image_save_func = 'ImageJPEG';
+                    $new_image_ext = 'jpg';
+                }
+
+                if($width > $height){ // Horizontal Rectangle?
+                    $x_pos = ($width - $height) / 2;
+                    $x_pos = ceil($x_pos);
+
+                    $y_pos = 0;
+                } else if($height > $width) {// Vertical Rectangle?
+                    $x_pos = 0;
+
+                    $y_pos = ($height - $width) / 2;
+                    $y_pos = ceil($y_pos);
+                }
 
 
-        // upload
-        // optimize the immage
-    }
+                switch($mode){
+                    case 'frame':
+                    if($width > $height){ // Horizontal Rectangle?
+                        $new_width = $width;
+                        $new_height = $width;
+                    }
+                    else if($height > $width){ // Vertical Rectangle?
+                        $new_width = $height;
+                        $new_height = $height;
+                    }
+                    break;
 
-    public static function upload($filePtr, $targetDIR, $fileName){
-        $imageFileType = strtolower(pathinfo($filePtr["name"], PATHINFO_EXTENSION));
-        $fullPath = $targetDIR.$fileName.".".$imageFileType;
+                    case 'crop':
+                    if($width > $height){ // Horizontal Rectangle?
+                        $new_width = $height;
+                        $new_height = $height;
+                    }
+                    else if($height > $width){ // Vertical Rectangle?
+                        $new_width = $width;
+                        $new_height = $width;
+                    }
 
-        //Check is actual image or not
-        if(!getimagesize($filePtr["tmp_name"])){
-            return "请上传正确的图像！"; // Reference: https://www.php.net/manual/en/function.getimagesize.php
+                    break;
+
+                    default:
+                }
+                $image = $image_create_func($imgPath);
+
+                $new_image = ImageCreate($new_width, $new_height);
+                $new_image = imagecolorallocate($new_image, 255, 255, 255);
+
+                // Crop to Square using the given dimensions
+                switch($mode){
+                    case 'frame':
+                    ImageCopy($new_image, $image, 0, 0, -$y_pos, -$x_pos, $new_width, $new_height);
+
+                    // $white = imagecolorallocate($new_image, 255, 255, 255);
+                    // if($width > $height){
+                    //     ImageFilledRectangle($new_image, 0, 0, $width, $y_pos, $white);
+                    //     ImageFilledRectangle($new_image, 0, 0 + $y_pos + $height, $width, $y_pos + $y_pos + $height, $white);
+                    // }else if($height > $width){
+                    //     ImageFilledRectangle($new_image, 0, 0, $x_pos, $height, $white);
+                    //     ImageFilledRectangle($new_image, 0 + $x_pos + $height, 0, $x_pos + $x_pos + $height, $height, $white);
+                    // }
+                    break;
+
+                    case 'crop':
+                    ImageCopy($new_image, $image, 0, 0, $x_pos, $y_pos, $width, $height);
+                    break;
+                }
+
+                // Save image
+                $process = $image_save_func($new_image, $imgPath) or die("There was a problem in saving the new file.");
+            }
         }
 
-        // Check file size
-        if ($filePtr["size"] > 20000000) {
-            echo "2";
-            return "图像大小过大！"; // If file too large
+        public static function optimizeImage($imgPath, $orgFileExtention){
+            $img = imagecreatefromjpeg($imgPath.".".$orgFileExtention);
+            imagejpeg ($img, $imgPath.".jpg", 75);
         }
 
-        // Allow certain file formats
-        // if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-        //   return "收据图像格式错误!";
-        // }
-        if($imageFileType != "jpg" && $imageFileType != "jpeg") {
-          return "收据图像格式错误!";
+        public static function uploadReceipt($filePtr, $orderId){
+            // To-do: if it is other image file, convert to jpg file
+            echo UsefulFunction::upload($filePtr, "assets/images/orders/", $orderId);
+            UsefulFunction::optimizeImage("assets/images/orders/".$orderId, strtolower(pathinfo($filePtr["name"], PATHINFO_EXTENSION)));
         }
 
-        // Upload file
-        if (!move_uploaded_file($filePtr["tmp_name"], $fullPath)) {
-            return "伺服器出错！请通过Whatapps联系客服来发送个人资料和单据。";
+        public static function uploadItemImage($filePtr, $o_id, $fileName, $mode){
+            // To-do: if it is other image file, convert to jpg file
+            $dir =  "assets/images/items/".$o_id."/";
+            $fullPath = $dir.$fileName;
+
+            UsefulFunction::upload($filePtr, $dir, $fileName);
+            UsefulFunction::optimizeImage($fullPath, strtolower(pathinfo($filePtr["name"], PATHINFO_EXTENSION)));
+            UsefulFunction::cropImageToSquare($fullPath, $mode);
+
         }
 
-        return "上传成功！";
+        public static function upload($filePtr, $targetDIR, $fileName){
+            $imageFileType = strtolower(pathinfo($filePtr["name"], PATHINFO_EXTENSION));
+            $fullPath = $targetDIR.$fileName.".".$imageFileType;
+
+            //Check is actual image or not
+            if(!getimagesize($filePtr["tmp_name"])){
+                return "请上传正确的图像！"; // Reference: https://www.php.net/manual/en/function.getimagesize.php
+            }
+
+            // Check file size
+            if ($filePtr["size"] > 20000000) {
+                echo "2";
+                return "图像大小过大！"; // If file too large
+            }
+
+            // Allow certain file formats
+            // if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+            //   return "收据图像格式错误!";
+            // }
+            if($imageFileType != "jpg" && $imageFileType != "jpeg") {
+                return "收据图像格式错误!";
+            }
+
+            // Upload file
+            if (!move_uploaded_file($filePtr["tmp_name"], $fullPath)) {
+                return "伺服器出错！请通过Whatapps联系客服来发送个人资料和单据。";
+            }
+
+            return "上传成功！";
+        }
+
     }
 
-}
-
-?>
+    ?>
