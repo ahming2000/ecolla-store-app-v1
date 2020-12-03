8a0d970415a353cb1a4557d15fe1405635fb1554
Neix20 <66442152+Neix20@users.noreply.github.com>
2020-12-03 16:16:50 +0800

    Fix the receipt (However, can't remove. Should be php side issue)

--- assets/block-user-page/cart-item-block.php
+++ assets/block-user-page/cart-item-block.php
@@ -1,4 +1,4 @@
-<div class="col-12 mb-3">
+<div class="col-12 mb-3" id="<?php echo $cartItem->getBarcode() ?>">
     <div class="card">
         <div class="card-body">
             <div class="row">
@@ -10,13 +10,16 @@
 
                 <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7 col-xl-7">
                     <div class="cart-item-info">
-                        <div class="item-title h4 font-weight-bold"><?php echo $cartItem->getItem()->getBrand()." ".$cartItem->getItem()->getName(); ?></div>
+                        <div class="item-title h4 font-weight-bold"><?php echo $cartItem->getItem()->getBrand() . " " . $cartItem->getItem()->getName(); ?></div>
                         <div class="item-properties h6 grey-text"><?php echo $cartItem->getItem()->getVarieties()[$cartItem->getVarietyIndex()]->getProperty(); ?></div>
                         <div class="item-note h6 grey-text"><?php echo $cartItem->getNote() ?></div>
-                        <div class="item-total-weight h6 grey-text"><?php $totalWeight = $cartItem->getQuantity() * $cartItem->getItem()->getVarieties()[$cartItem->getVarietyIndex()]->getWeight(); echo $totalWeight."kg" ?></div>
+                        <div class="item-total-weight h6 grey-text"><?php $totalWeight = $cartItem->getQuantity() * $cartItem->getItem()->getVarieties()[$cartItem->getVarietyIndex()]->getWeight();
+                                                                    echo $totalWeight . "kg" ?></div>
 
                         <div class="row d-flex justify-content-between align-items-center">
-                            <div class="col-xs-12 col-sm-12 col-md-3"><div class="h6">数量：</div></div>
+                            <div class="col-xs-12 col-sm-12 col-md-3">
+                                <div class="h6">数量：</div>
+                            </div>
                             <div class="col-xs-12 col-sm-12 col-md-9 quantity-button-control data-update-enable">
                                 <button type="button" class="btn btn-primary decrease-button btn-sm mx-3 my-3">-</button>
                                 <input type="number" class="mx-3 my-3 quantity-number" value="<?php echo $cartItem->getQuantity(); ?>" style="width: 45px" disabled>
@@ -34,3 +37,78 @@
         </div>
     </div>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+<script>
+    $(function() {
+        let cartItem_obj = {
+            barcode: "<?php echo $cartItem->getBarcode() ?>",
+            quantity: 1
+        };
+
+        $("#<?php echo $cartItem->getBarcode() ?> .quantity-button-control button").on("click", function() {
+            let price = <?php echo $cartItem->getSubPrice() ?>,
+                quantity = <?php echo $cartItem->getQuantity() ?>,
+                pricePerItem = price / quantity;
+
+            let MAX_COUNT = 10;
+
+            var count = $(this).parent().children('input').val();
+
+            if ($(this).hasClass("increase-button")) {
+                $(this).parent().children('input').val(++count);
+
+                if ($(this).parent().children('input').val() == MAX_COUNT) {
+                    $(this).attr('disabled', 'disabled');
+                    $(this).parent().children('.decrease-button').removeAttr('disabled');
+                } else {
+                    $(this).removeAttr('disabled');
+                    $(this).parent().children('.decrease-button').removeAttr('disabled');
+                }
+
+                $.ajax({
+                    type: "POST",
+                    url: 'cart.php',
+                    dataType: 'json',
+                    data: {
+                        addItemQuantity: JSON.stringify(cartItem_obj)
+                    }
+                });
+            } else if ($(this).hasClass("decrease-button")) {
+                $(this).parent().children('input').val(--count);
+
+                if ($(this).parent().children('input').val() == 1) {
+                    $(this).parent().children('.increase-button').removeAttr('disabled');
+                    $(this).attr('disabled', 'disabled');
+                } else {
+                    $(this).parent().children('.increase-button').removeAttr('disabled');
+                    $(this).removeAttr('disabled');
+                }
+
+                $.ajax({
+                    type: "POST",
+                    url: 'cart.php',
+                    dataType: 'json',
+                    data: {
+                        minusItemQuantity: JSON.stringify(cartItem_obj)
+                    }
+                });
+            }
+
+            $("#<?php echo $cartItem->getBarcode() ?> .item-price").html(`RM${(pricePerItem * $(this).parent().children('input').val()).toFixed(2)}`);
+
+        });
+
+        $("#<?php echo $cartItem->getBarcode() ?> .remove-item-button").on("click", function() {
+            $.ajax({
+                type: "POST",
+                url: 'cart.php',
+                dataType: 'json',
+                data: {
+                    removeItem: JSON.stringify(cartItem_obj)
+                }
+            });
+            $("#<?php echo $cartItem->getBarcode() ?>").remove();
+        });
+    });
+</script>
--- cart.php
+++ cart.php
@@ -1,76 +1,100 @@
-<?php include "assets/includes/class-auto-loader.inc.php"; //Auto include classes when needed. ?>
-<?php $cart = new Cart(); $view = new View(); //Must declare before html tag for php cookie ?>
+<?php include "assets/includes/class-auto-loader.inc.php"; //Auto include classes when needed. 
+?>
+<?php $cart = new Cart();
+$view = new View(); //Must declare before html tag for php cookie 
+?>
 <?php
-    if(isset($_POST["clearCart"])){
-        $cart->resetCart();
-    }
+if (isset($_POST["clearCart"])) {
+    $cart->resetCart();
+}
+
+if (isset($_POST['addItemQuantity'])) {
+    $obj = json_decode($_POST['addItemQuantity']);
+    $cart->editQuantity($obj->barcode, 1);
+}
+
+if (isset($_POST['minusItemQuantity'])) {
+    $obj = json_decode($_POST['minusItemQuantity']);
+    $cart->editQuantity($obj->barcode, -1);
+}
+
+if (isset($_POST['removeItem'])) {
+    $obj = json_decode($_POST['removeItem']);
+    $cart->removeItem($obj->barcode);
+}
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php $title = "购物车 | Ecolla ε口乐"; include "assets/includes/stylesheet-script-declaration.inc.php" ?>
-        <!-- To-do: Meta for google searching -->
-    </head>
 
-    <body>
-        <?php $c = $cart; include "assets/block-user-page/header.php"; ?>
+<head>
+    <?php $title = "购物车 | Ecolla ε口乐";
+    include "assets/includes/stylesheet-script-declaration.inc.php" ?>
+    <!-- To-do: Meta for google searching -->
+</head>
+
+<body>
+    <?php $c = $cart;
+    include "assets/block-user-page/header.php"; ?>
 
     <wrapper class="d-flex flex-column">
-    <main class="flex-fill"> <!--put content-->
+        <main class="flex-fill">
+            <!--put content-->
 
-        <div class="container mt-4">
-            <div class="row">
-                <div class="col-lg-8">
-                    <div class="card mb-2">
-                        <div class="card-body" id="cartItemList">
-                            <?php
+            <div class="container mt-4">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="card mb-2">
+                            <div class="card-body" id="cartItemList">
+                                <?php
 
                                 $cartList = $cart->getCartItems();
 
-                                if(empty($cartList[0])) echo "<div class=\"text-center\"><img src=\"assets/images/icon/empty-cart.png\" width=\"150\" height=\"150\"> <h5 class=\"p-2\">您的购物车为空</h5></div>";
+                                if (empty($cartList[0])) echo "<div class=\"text-center\"><img src=\"assets/images/icon/empty-cart.png\" width=\"150\" height=\"150\"> <h5 class=\"p-2\">您的购物车为空</h5></div>";
 
-                                for($i = 0; $i < sizeof($cartList); $i++){
+                                for ($i = 0; $i < sizeof($cartList); $i++) {
                                     $cartItem = $cartList[$i];
 
                                     include "assets/block-user-page/cart-item-block.php";
                                 }
-                                if(isset($cartList[0])){
+                                if (isset($cartList[0])) {
                                     echo "<div class=\"col-12\"><form action=\"\" method=\"post\"><button class=\"btn btn-primary btn-block\" name=\"clearCart\" type=\"submit\">清空购物车</button></form></div>";
                                 }
-                            ?>
+                                ?>
 
 
+                            </div>
                         </div>
-                    </div>
-                    <div class="card mb-3">
-                        <div class="card-body bg-warning">
-                            <!-- Delivery Description -->
-                            <h5>邮寄服务</h5>
-                            <p class="text-muted">唯独金宝区免邮。</p>
+                        <div class="card mb-3">
+                            <div class="card-body bg-warning">
+                                <!-- Delivery Description -->
+                                <h5>邮寄服务</h5>
+                                <p class="text-muted">唯独金宝区免邮。</p>
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <?php $c = $cart;
+                                include "assets/block-user-page/order-summary-block.php"; ?>
+                                <form action="check-out.php" method="post">
+                                    <input class="btn btn-primary btn-block" type="submit" value="前往付款">
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <?php $c = $cart; include "assets/block-user-page/order-summary-block.php"; ?>
-                            <form action="check-out.php" method="post">
-                                <input class="btn btn-primary btn-block" type="submit" value="前往付款">
-                            </form>
-                        </div>
-                    </div>
 
-                </div>
             </div>
 
-        </div>
-
-    </main>
+        </main>
 
         <?php include "assets/block-user-page/footer.php"; ?>
 
     </wrapper>
 
-    </body>
+</body>
+
 </html>
