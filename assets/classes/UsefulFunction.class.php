e0d371be2a69d3f87afa4a88250bb64d0ef9f522
AhMing <keeshengming@outlook.com>
2020-12-04 21:34:09 +0800

    Remove cart item bug fixed.

--- assets/classes/UsefulFunction.class.php
+++ assets/classes/UsefulFunction.class.php
@@ -3,32 +3,36 @@
 class UsefulFunction{
 
     public static function removeArrayElementE($array, $element){
-        //Get the index of the element that require to remove
+
+        // Unset the selected element to remove
         for($i = 0; $i < sizeof($array); $i++){
-            if(is_null($value)){
-                $index = $i;
+            if($array[$i] === $element){ //Compare object
+                unset($array[$i]); // This function will break the array with hole (discontinued key value)
                 break;
             }
         }
-        //Delete the element from array (will left a null space)
-        $array = array_diff($array, $element);
-        //Copy the array part after the null space to the end
-        $arrayAfterElement = array_slice($array, $index + 1, length($array));
-        //Replace the array element (from null space to the last element) with elements after null space
-        array_splice($array, $index, length($array), $arrayAfterElement);
-        return $array; //To do (Debug): if the array do not change by using array_splice(), need to address the result using return
+
+        // Rearrange to fix discountinued key value
+        $newArray = array();
+        foreach($array as $e){
+            array_push($newArray, $e);
+        }
+
+        return $newArray;
     }
 
     public static function removeArrayElementI($array, $index){
-        //Track back the array element
-        $element = $array[$index];
-        //Delete the element from array (will left a null space)
-        $array = array_diff($array, $element);
-        //Copy the array part after the null space to the end
-        $arrayAfterElement = array_slice($array, $index + 1, length($array));
-        //Replace the array element (from null space to the last element) with elements after null space
-        array_splice($array, $index, length($array), $arrayAfterElement);
-        return $array; //To do (Debug): if the array do not change by using array_splice(), need to address the result using return
+
+                // Unset the selected element to remove
+                unset($array[$index]); // This function will break the array with hole (discontinued key value)
+
+                // Rearrange to fix discountinued key value
+                $newArray = array();
+                foreach($array as $e){
+                    array_push($newArray, $e);
+                }
+
+                return $newArray;
     }
 
     public static function startsWith ($string, $startString) {
