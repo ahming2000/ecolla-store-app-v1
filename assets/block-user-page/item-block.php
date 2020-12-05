4cb21185f3fb0ed3ad76abdf2128629dc433d4e8
AhMing <keeshengming@outlook.com>
2020-12-05 14:23:06 +0800

    Change all format from png to jpg

--- assets/block-user-page/item-block.php
+++ assets/block-user-page/item-block.php
@@ -1,7 +1,7 @@
 <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
     <div class="card">
         <a href="items/<?php echo $item->getBrand() ?>-<?php echo $item->getName() ?>.php">
-            <img src="assets/images/items/<?php echo $i_id; ?>/0.png" class="card-img-top" alt="image">
+            <img src="assets/images/items/<?php echo $i_id; ?>/0.jpg" class="card-img-top" alt="image">
         </a>
         <div class="card-body">
             <h5 class="card-title"><?php echo $item->getName() ?></h5>
