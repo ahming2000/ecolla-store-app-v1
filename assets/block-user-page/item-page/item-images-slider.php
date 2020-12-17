d178e8f461e4292b6d8b32fead835d07ee6042c7
J0u0J <72692004+J0u0J@users.noreply.github.com>
2020-12-17 13:15:17 +0800

    floating button on both side of img

--- assets/block-user-page/item-page/item-images-slider.php
+++ assets/block-user-page/item-page/item-images-slider.php
@@ -1,9 +1,29 @@
+<style>/*can move it to css file later, just for demo*/
+.btn1 {
+  position: absolute;
+  top: 50%;
+  right: 90%;
+  background-color: rgba(71, 71, 107, 0.5);
+  border: none;
+  font-size: 20px;
+  cursor: pointer;
+  z-index: 2;
+}
+.btn2{
+  position: absolute;
+  top: 50%;
+  left: 90%;
+  background-color: rgba(71, 71, 107, 0.5);
+  border: none;
+  font-size: 20px;
+  cursor: pointer;
+  z-index: 2;
+}
+</style>
+
 <div class="col-12 mb-3">
-<div class="row">
-<div class="col-2">
-<div class="slider-nav-control-prev"><img width="50" height="50" src="../assets/images/alt/prev-button-alt.png" /></div>
-</div>
-<div class="col-8">
+    <div class="btn1 slider-nav-control-prev"><img class="img-fluid" src="../assets/images/alt/prev-button-alt.png" /></div>
+    <div class="btn2 slider-nav-control-next"><img class="img-fluid" src="../assets/images/alt/next-button-alt.png" /></div>
     <div class="slider-container">
         <?php for($i = 0; $i < $item->getImgCount(); $i++) : ?>
             <img class="img-fluid general-img" src="../assets/images/items/<?= $i_id; ?>/<?= $i; ?>.jpg"/>
@@ -14,11 +34,6 @@
                 <img class="img-fluid variety-img" id="img-<?= $variety->getBarcode(); ?>" src="../assets/images/items/<?= $i_id; ?>/<?= $variety->getBarcode(); ?>.jpg"/>
             <?php endif; ?>
         <?php endforeach; ?>
-    </div>
-    </div>
-    <div class="col-2">
-    <div class="slider-nav-control-next"><img width="50" height="50" src="../assets/images/alt/next-button-alt.png" /></div>
-    </div>
     </div>
 </div>
 
