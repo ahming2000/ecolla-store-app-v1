cb1109b2eef919126f52f9b5e58043d7652ce09a
AhMing <keeshengming@outlook.com>
2020-12-22 19:35:35 +0800

    Combine order summary block into cart.

--- cart.php
+++ cart.php
@@ -131,7 +131,25 @@ if(isset($_POST['changeRegion'])){
                 <!-- Order summary -->
                 <div class="card mb-3">
                     <div class="card-body">
-                        <?php include "assets/block-user-page/order-summary-block.php"; ?>
+
+                        <h5 class="card-title">购物车订单摘要</h5>
+
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0">
+                                小计
+                                <span>RM <?= number_format($cart->getSubtotal(), 2); ?></span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center px-0">
+                                邮费
+                                <span>RM <?= number_format($cart->getShippingFee(), 2); ?></span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0">
+                                <strong>总计</strong>
+                                <?php $total = $cart->getSubtotal() + $cart->getShippingFee(); ?>
+                                <span><strong>RM <?= number_format($total, 2); ?></strong></span>
+                            </li>
+                        </ul>
+
                         <form action="check-out.php" method="post">
                             <button class="btn btn-primary btn-block" type="submit" id="submit_btn">前往付款</button>
                         </form>
@@ -144,6 +162,18 @@ if(isset($_POST['changeRegion'])){
     </main>
 
     <script>
+
+    </script>
+
+    <script>
+
+        // Make sure submit button is disable when there is no item (total = 0) in the cart
+        $(function() {
+            let price = <?= number_format($total, 2); ?>;
+            if(price <= 0)
+                $("#submit_btn").attr("disabled", "disabled");
+        });
+
         // Region settings scripts
         var isEastMY = <?php echo $cart->isEastMY(); ?>;
         if(isEastMY){
