08bab37036e02ba012cdb6fa424370424ab934de
AhMing <keeshengming@outlook.com>
2021-01-05 20:35:35 +0800

    Remove residual code.

--- assets/block-admin-page/catogory-from-block.php
+++
@@ -1,9 +0,0 @@
-<div class="form-row">
-    <div class="col-xs-2 col-sm-4 col-md-3 col-lg-4 text-sm-left text-md-right mb-3">
-        <label for="brand">商品类别/标签 1：</label>
-    </div>
-
-    <div class="col-xs-10 col-sm-8 col-md-9 col-lg-8 mb-3 text-center">
-        <input type="text" class="form-control mb-3" name="catogory" aria-describedby="catogory" list="catogoryList" />
-    </div>
-</div>
--- assets/block-user-page/breadcrumb-block.php
+++
@@ -1,8 +0,0 @@
-<div class="container mt-4">
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="../../item-list.php">商品列表</a></li>
-            <li class="breadcrumb-item active" aria-current="page"><?php echo $i->getBrand()." ".$i->getName(); ?></li>
-        </ol>
-    </nav>
-</div>
