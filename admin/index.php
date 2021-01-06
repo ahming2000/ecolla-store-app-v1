04faa38514e68fce0e2481cbe6622979b8b64514
Neix20 <66442152+Neix20@users.noreply.github.com>
2021-01-07 00:24:02 +0800

    Make some changes to dashboard

--- admin/index.php
+++ admin/index.php
@@ -43,9 +43,11 @@ function updateCartItemList()
     foreach ($GLOBALS['report_order'] as $o) {
         $tmp_no_clone_cart = $o->getCart()->getCartItems();
         $tmp_cart = array();
+
         foreach ($tmp_no_clone_cart as $cartItem) {
             array_push($tmp_cart, clone $cartItem);
         }
+
         if (empty($GLOBALS['cartItems'])) {
             $GLOBALS['cartItems'] = $tmp_cart;
         } else {
@@ -146,24 +148,34 @@ if (isset($_POST["report_date"])) {
         /*Indigo: */
         /*Purple:  #FF00FF, #EE82EE, #DA70D6, #9400D3, #8B008B */
         .piechart {
-            width: 300px;
-            height: 300px;
+            width: 400px;
+            height: 400px;
             border-radius: 50%;
-            background-image: conic-gradient(
-                <?php
-                    $cur_deg = 0;
-                    $str = "";
-                    for ($i = 0; $i < count($cartItems); $i++) {
-                        $str .= $color[$i] . " " . $cur_deg . "deg ";
-                        $cur_deg += $cartItems[$i]->getQuantity() / $total_sold * 360;
-                        $str .= $cur_deg . "deg,";
-                    }
+            background-image: conic-gradient(<?php
+                                                $cur_deg = 0;
+                                                $str = "";
+                                                for ($i = 0; $i < count($cartItems); $i++) {
+                                                    $str .= $color[$i] . " " . $cur_deg . "deg ";
+                                                    $cur_deg += $cartItems[$i]->getQuantity() / $total_sold * 360;
+                                                    $str .= $cur_deg . "deg,";
+                                                }
 
-                    echo substr($str, '0', '-1');
+                                                echo substr($str, '0', '-1');
 
-                ?>);
+                                                ?>);
         }
     </style>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script>
+        $(function() {
+            let t_date = new Date(<?php echo strtotime($t_date) ?> * 1000);
+            $("#date_").val(t_date.toLocaleDateString('en-CA'));
+            $("#date_").change(e => {
+                $("#form_report_date").submit();
+            });
+        });
+    </script>
 </head>
 
 <body>
@@ -172,50 +184,65 @@ if (isset($_POST["report_date"])) {
 
     <header><?php include "../assets/block-admin-page/header.php"; ?></header>
 
+    <br /><br /><br />
+
     <!--Daily report -->
     <div class="container">
         <div class="text-center h1">Today Sales report</div>
         <!--Add a function where you can sort by monthly daily or annual sales report -->
         <hr style="height:2.5px;border:none;color:#333;background-color:#333;" />
 
-        <div class="row">
-            <div class="col-6 p-3">
-                <div class="border border-primary p-3" style="height: 450px;">
+        <form method="POST" action="../admin/index.php" id="form_report_date">
+            <div class="row text-center h4">
+                <div class="col-4">Date: <input type="date" name="report_date" id="date_" max="<?php echo date('Y-m-d') ?>" />
+                </div>
+                <div class="col-4">Total Sales Revenue: RM <?php echo $total_price; ?></div>
+                <div class="col-4">Total number of items sold: <?php echo $total_sold; ?></div>
+            </div>
+        </form>
 
-                    <div class="d-flex justify-content-center align-items-center border border-danger mb-3" style="height: 330px">
+        <div class="row">
+            <div class="col-8 p-3">
+                <div class="border border-success" style="height: 475px;">
+                    <div class="bg-success h2 text-center" style="color: white;">Pie Chart</div>
+                    <div class="d-flex justify-content-center align-items-center pt-1">
                         <div class="piechart"></div>
                     </div>
-                    <div class="d-flex justify-content-center align-items-center border border-danger" style="height: 75px">
-                        <!--Legend for pie chart-->
-                        <table>
-                            <tbody>
-                                <?php
-                                    $limit = (ceil(count($cartItems) / 2) < 4) ? ceil(count($cartItems) / 2)  : 4;
-                                    $str = "<tr>";
-                                    for($i = 0; $i < count($cartItems); $i++){
-                                        if($i % $limit == 0 && $i != 0){
-                                            $str .= "</tr><tr>";
-                                        }
-                                        $str .= "<td style='font-size: 20px;background-color: ".$color[$i]."'>" . $cartItems[$i]->getItem()->getVarieties()[$cartItem->getVarietyIndex()]->getProperty() . "</td>";
-                                    }
-                                    echo substr($str, '0', '-5');
-                                ?>
-                            </tbody>
-                        </table>
+                </div>
+            </div>
+            <div class="col-4 p-3">
+                <div class="border border-success" style="height: 475px;">
+                    <!--Legend for pie chart-->
+                    <div class="px-3 text-center">
+
+                        <div class="row bg-success">
+                            <div class="col">
+                                <div class="h3" style="color: white;">Legend</div>
+                            </div>
+                        </div>
+
+                        <div class="row table-success">
+                            <div class="col-2">CCode</div>
+                            <div class="col-4">Name</div>
+                            <div class="col-3">Variety</div>
+                            <div class="col-2">Count</div>
+                        </div>
+                        <?php
+                            for ($i = 0; $i < count($cartItems); $i++) {
+                                $str = "<div class='row'>"; 
+                                $color_str = "<div class='col-2'>
+                                    <div style='width: 30px; height: 30px; background-color: " . $color[$i] . "'></div>
+                                </div>";
+                                $name_str = "<div class='col-4'>" . $cartItems[$i]->getItem()->getName() . "</div>";
+                                $variety_str = "<div class='col-3'>" . $cartItems[$i]->getItem()->getVarieties()[$cartItem->getVarietyIndex()]->getProperty() . "</div>";
+                                $count_str = "<div class='col-2'>" . $cartItems[$i]->getQuantity() . "</div>";
+                                $str .= $color_str . $name_str . $variety_str . $count_str . "</div>";
+                                echo $str;
+                            }
+                        ?>
                     </div>
                 </div>
             </div>
-            <div class="col-6 p-3">
-                <div class="border border-success" style="height: 450px;">
-                    <!--Put Graph-->
-                </div>
-            </div>
-        </div>
-
-        <div class="row text-center h4">
-            <div class="col-4">Date: <?php echo $t_date; ?></div>
-            <div class="col-4">Total Sales Revenue: RM <?php echo $total_price; ?></div>
-            <div class="col-4">Total number of items sold: <?php echo $total_sold; ?></div>
         </div>
 
         <table class="table text-center border border-dark">
@@ -235,7 +262,7 @@ if (isset($_POST["report_date"])) {
                 <?php
                 $tmp = 0;
                 foreach ($report_order as $o) {
-                    echo "<tr class='table-danger text-center'>" .
+                    echo "<tr class='table-danger'>" .
                         "<td colspan='8'>" . $o->getOrderId() . "</td>" .
                         "</tr>";
 
@@ -260,15 +287,6 @@ if (isset($_POST["report_date"])) {
 
     </div>
 
-
-    <div class="container">
-        <form method="POST" action="../admin/index.php">
-            <input type="date" name="report_date" />
-            <!--Make a javascript for submission? -->
-            <input type="submit" value="submit" />
-        </form>
-    </div>
-
     <div class="container">
         <br>
         <?php
@@ -279,5 +297,16 @@ if (isset($_POST["report_date"])) {
         ?>
     </div>
 </body>
+
+<!--<div> <?php
+            // $str = "<tr>";
+            // for ($i = 0; $i < count($cartItems); $i++) {
+            //     if ($i % $limit == 0 && $i != 0) {
+            //         $str .= "</tr><tr>";
+            //     }
+            //     $str .= "<td style='font-size: 20px;background-color: " . $color[$i] . "'>" . $cartItems[$i]->getItem()->getVarieties()[$cartItem->getVarietyIndex()]->getProperty() . "</td>";
+            // }
+            // echo substr($str, '0', '-5');
+            ?></div> -->
 
 </html>
