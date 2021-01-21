2e7546775df134c1e0e3351fdf845c387c3b59a8
AhMing <keeshengming@outlook.com>
2021-01-21 17:10:39 +0800

    Add pagination and search function

--- admin/item-management.php
+++ admin/item-management.php
@@ -15,8 +15,24 @@ include "../assets/includes/class-auto-loader.inc.php";
 $view = new View();
 $controller = new Controller();
 
+// Calculate value for pagination
+$MAX_ITEMS = $view->getMaxManageContent();
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$page = is_numeric($page) ? $page : 0 ; // Avoid non number value in url
+$start = ($page - 1) * $MAX_ITEMS;
+
 //Get item information
-$itemList = $view->getAllItems();
+$keywordSearch = isset($_GET['search']) ? $_GET['search'] : "";
+$names = $view->itemManagementFilter($keywordSearch);
+$itemCount = sizeof($names);
+$totalPage = ceil($itemCount / $MAX_ITEMS);
+
+$itemList = array();
+for($i = $start; $i < $start + $MAX_ITEMS; $i++){
+    if(isset($names[$i])){
+        $itemList[] = $view->getItem($names[$i]);
+    }
+}
 
 /* Operation */
 
@@ -61,7 +77,7 @@ if (isset($_POST['delete'])) {
 
 <head>
     <?php include "../assets/includes/stylesheet.inc.php"; ?>
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
 
     <!--Load Table functions from admin_table.js-->
@@ -164,14 +180,12 @@ if (isset($_POST['delete'])) {
 
     <header><?php include "../assets/block-admin-page/header.php"; ?></header>
 
-    <div style="margin-top: 100px;"></div>
-
-    <div class="container">
+    <main class="container">
         <div class="h1">商品管理</div>
+
         <form action="" method="post" id="action-form">
             <div class="row p-2">
                 <input type="text" name="name" id="selectedName" value="" hidden />
-                <input type="text" name="brand" id="selectedBrand" value="" hidden />
                 <div class="mr-2" style="width: 30%"><button onclick="addButtonClicked()" type="button" class="btn btn-primary btn-block btn-text" name="addButton" id="addButton">添加商品</button></div>
                 <div class="mr-2" style="width: 30%"><button onclick="editButtonClicked()" type="button" class="btn btn-primary btn-block btn-text" name="editButton" id="editButton" disabled>编辑商品</button></div>
                 <div style="width: 30%"><button onclick="deleteButtonClicked()" type="submit" class="btn btn-primary btn-block btn-text" name="deleteButton" id="deleteButton" disabled>删除商品</button></div>
@@ -185,6 +199,42 @@ if (isset($_POST['delete'])) {
                 <div id="detail_board" style="height: 240px; width: 150px; background-color: white; display: none;position: absolute; z-index: 2; right: 0px;">
                 </div>
             </div>
+        </div>
+
+        <div class="row mb-3">
+            <div class="col-6">
+                <form action="" method="get">
+
+                    <div class="form-row">
+                        <div class="col-10">
+                            <!-- Item searching -->
+                            <input type="text" class="form-control" maxlength="20" name="search" value="<?= isset($_GET["search"]) ? $_GET["search"] : ""; ?>" />
+                        </div>
+                        <div class="col-2">
+                            <input type="submit" class="btn btn-primary p-2 mt-0" value="搜索"/>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+            <div class="col-6">
+                <nav>
+                    <ul class="pagination justify-content-center">
+                        <li class="page-item <?= $page == 1 ? "disabled" : ""; ?>">
+                            <a class="page-link" id="previous-page-button" <?= $page == 1 ? "tabindex='1' aria-disabled='true'" : ""; ?>>上一页</a>
+                        </li>
+
+                        <?php for($i = 1; $i <= $totalPage; $i++) : ?>
+                            <li class="page-item <?= $page == $i ? "active" : ""; ?>" value="<?= $i; ?>"><a class="page-link page-number-link"><?= $i; ?></a></li>
+                        <?php endfor; ?>
+
+                        <li class="page-item<?= $page == $totalPage ? " disabled" : ""; ?>">
+                            <a class="page-link" id="next-page-button" <?= $page == $totalPage ? "tabindex='1' aria-disabled='true'" : ""; ?>>下一页</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
         </div>
 
         <table class="table table-bordered mt-3" id="item-table">
@@ -209,95 +259,110 @@ if (isset($_POST['delete'])) {
             </tbody>
         </table>
 
-        <script>
-            var selectedCount = 0;
-            var selectedRowIndex = null;
+    </main>
 
-            function itemCheckBoxClicked(source) {
+    <script>
+        var selectedCount = 0;
+        var selectedRowIndex = null;
 
-                if (source.checked) {
-                    selectedCount++;
-                } else {
-                    selectedCount--;
-                }
+        function itemCheckBoxClicked(source) {
 
-                //Check to disable the edit button (Website cannot support multiple item editing)
-                if (selectedCount != 1) {
-                    document.getElementById("editButton").setAttribute("disabled", "disabled");
-                    document.getElementById("deleteButton").setAttribute("disabled", "disabled");
-                } else {
-                    document.getElementById("editButton").removeAttribute("disabled");
-                    document.getElementById("deleteButton").removeAttribute("disabled");
-                }
-
-                //set the row selected
-                if (selectedCount == 1) {
-                    //Loop throught the class
-                    for (var i = 0; i < document.getElementsByClassName('item-check-box').length; i++) {
-                        if (document.getElementsByClassName('item-check-box')[i].checked) {
-                            selectedRowIndex = i + 1;
-                        }
-                    }
-                    // Must use [0] to get first class selected
-                    document.getElementById("selectedName").value = source.parentNode.getElementsByClassName("infoBoxItemName")[0].value;
-                    document.getElementById("selectedBrand").value = source.parentNode.getElementsByClassName("infoBoxItemBrand")[0].value;
-                } else {
-                    selectedRowIndex = null;
-                    document.getElementById("selectedName").value = "";
-                    document.getElementById("selectedBrand").value = "";
-                }
-
+            if (source.checked) {
+                selectedCount++;
+            } else {
+                selectedCount--;
             }
 
-            function selectAll(source) {
-                checkboxes = document.getElementsByName('item-check-box');
-                for (var i = 0, n = checkboxes.length; i < n; i++) {
-                    checkboxes[i].checked = source.checked;
-                }
-
-                if (source.checked) {
-                    selectedCount = document.getElementsByClassName('item-check-box').length;
-                    document.getElementById("editButton").setAttribute("disabled", "disabled");
-                    document.getElementById("deleteButton").setAttribute("disabled", "disabled");
-                } else {
-                    selectedCount = 0;
-                    document.getElementById("editButton").setAttribute("disabled", "disabled");
-                    document.getElementById("deleteButton").setAttribute("disabled", "disabled");
-                    document.getElementById("selectedName").value = "";
-                    document.getElementById("selectedBrand").value = "";
-                }
+            //Check to disable the edit button (Website cannot support multiple item editing)
+            if (selectedCount != 1) {
+                document.getElementById("editButton").setAttribute("disabled", "disabled");
+                document.getElementById("deleteButton").setAttribute("disabled", "disabled");
+            } else {
+                document.getElementById("editButton").removeAttribute("disabled");
+                document.getElementById("deleteButton").removeAttribute("disabled");
             }
 
-
-            var myTable = document.getElementById("item-table");
-
-            function untickAll() {
-                //Untick all selection
+            //set the row selected
+            if (selectedCount == 1) {
+                //Loop throught the class
                 for (var i = 0; i < document.getElementsByClassName('item-check-box').length; i++) {
                     if (document.getElementsByClassName('item-check-box')[i].checked) {
-                        document.getElementsByClassName('item-check-box')[i].checked = false;
+                        selectedRowIndex = i + 1;
                     }
                 }
+                // Must use [0] to get first class selected
+                document.getElementById("selectedName").value = source.parentNode.getElementsByClassName("infoBoxItemName")[0].value;
+                document.getElementById("selectedBrand").value = source.parentNode.getElementsByClassName("infoBoxItemBrand")[0].value;
+            } else {
+                selectedRowIndex = null;
+                document.getElementById("selectedName").value = "";
+                document.getElementById("selectedBrand").value = "";
             }
 
-            function addButtonClicked() {
-                untickAll();
-                document.location.href = "item-create.php";
+        }
+
+        function selectAll(source) {
+            checkboxes = document.getElementsByName('item-check-box');
+            for (var i = 0, n = checkboxes.length; i < n; i++) {
+                checkboxes[i].checked = source.checked;
             }
 
-            function editButtonClicked() {
-                untickAll();
-
-                document.location.href = "item-edit.php?" +
-                    "itemName=" + document.getElementById("selectedName").value;
+            if (source.checked) {
+                selectedCount = document.getElementsByClassName('item-check-box').length;
+                document.getElementById("editButton").setAttribute("disabled", "disabled");
+                document.getElementById("deleteButton").setAttribute("disabled", "disabled");
+            } else {
+                selectedCount = 0;
+                document.getElementById("editButton").setAttribute("disabled", "disabled");
+                document.getElementById("deleteButton").setAttribute("disabled", "disabled");
+                document.getElementById("selectedName").value = "";
+                document.getElementById("selectedBrand").value = "";
             }
+        }
 
-            function deleteButtonClicked() {
-                untickAll();
+
+        var myTable = document.getElementById("item-table");
+
+        function untickAll() {
+            //Untick all selection
+            for (var i = 0; i < document.getElementsByClassName('item-check-box').length; i++) {
+                if (document.getElementsByClassName('item-check-box')[i].checked) {
+                    document.getElementsByClassName('item-check-box')[i].checked = false;
+                }
             }
-        </script>
+        }
 
-    </div>
+        function addButtonClicked() {
+            untickAll();
+            document.location.href = "item-create.php";
+        }
+
+        function editButtonClicked() {
+            untickAll();
+
+            document.location.href = "item-edit.php?" +
+                "itemName=" + document.getElementById("selectedName").value;
+        }
+
+        function deleteButtonClicked() {
+            untickAll();
+        }
+
+        $(document).ready(function(){
+
+            $("#previous-page-button").on("click", function(){
+                window.location.href = "item-management.php?<?= isset($_GET["search"]) ? "search=" . $_GET["search"] . "&" : ""; ?>page=<?= ($page - 1) ?>";
+            });
+
+            $("#next-page-button").on("click", function(){
+                window.location.href = "item-management.php?<?= isset($_GET["search"]) ? "search=" . $_GET["search"] . "&" : ""; ?>page=<?= ($page + 1) ?>";
+            });
+
+            $(".page-number-link").on("click", function(e){
+                window.location.href = "item-management.php?<?= isset($_GET["search"]) ? "search=" . $_GET["search"] . "&" : ""; ?>page=" + $(this).parent().val();
+            });
+        });
+    </script>
 </body>
 
 </html>
